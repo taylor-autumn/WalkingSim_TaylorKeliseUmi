54919d3e88c6a4194404cd2c780e154af1ddbee7
taylor-autumn <taylorastuff@gmail.com>
2026-01-27 14:17:02 -0500

    changes

---
+++ Walking Sim - KUT/Assets/Scripts/unitInfo.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unitInfo : MonoBehaviour
+{
+    public string name;
+    public List<string> firstLines;
+    public List<string> secondLines;
+    public List<string> thirdLines;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
