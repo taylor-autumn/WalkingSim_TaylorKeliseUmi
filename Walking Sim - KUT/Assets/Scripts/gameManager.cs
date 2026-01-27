0997bc308bd0d61510ef81c58dc3ebac9bf3ed78
taylor-autumn <taylorastuff@gmail.com>
2026-01-27 11:46:32 -0500

    wsedrftgyhuji

--- Walking Sim - KUT/Assets/Scripts/gameManager.cs
+++ Walking Sim - KUT/Assets/Scripts/gameManager.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
+public enum gameState { normalMode, interactMode}
 public class gameManager : MonoBehaviour
 {
+    public gameState state;
+    //this script is where we interact and shit
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        state=gameState.normalMode;
     }
 
     // Update is called once per frame
