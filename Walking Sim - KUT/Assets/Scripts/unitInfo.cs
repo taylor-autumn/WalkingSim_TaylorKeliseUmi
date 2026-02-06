375a096c6c8b1a364cbcd409cc1f8215e2179ea4
taylor-autumn <taylorastuff@gmail.com>
2026-02-06 10:25:39 -0500

    waesrdtfyguhijok

--- Walking Sim - KUT/Assets/Scripts/unitInfo.cs
+++ Walking Sim - KUT/Assets/Scripts/unitInfo.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 public class unitInfo : MonoBehaviour
 {
     public string charName;
+    [TextArea(2,2)]
     public List<string> firstLines;
+    [TextArea(2, 2)]
     public List<string> secondLines;
+    [TextArea(2, 2)]
     public List<string> thirdLines;
+    [TextArea(2, 2)]
     public List<string> fourthLines;
+    [TextArea(2, 2)]
     public List<string> fifthLines;
+    [TextArea(2, 2)]
     public List<string> sixthLines;
 
-    //public Queue<string> level1;
+    //public Queue<string> level1; tutorial stuff
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
