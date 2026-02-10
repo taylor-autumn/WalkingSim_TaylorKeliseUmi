6a814430816583fa3b70bfcfed7b09ad5063ccbd
taylor-autumn <taylorastuff@gmail.com>
2026-02-10 11:41:55 -0500

    ftgyhujiko

---
+++ Walking Sim - KUT/Assets/Scripts/teleport.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public GameObject respawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        other.transform.position = respawnPoint.transform.position;
+    //        print("teleporter activated");
+    //    }
+    //}
+}
